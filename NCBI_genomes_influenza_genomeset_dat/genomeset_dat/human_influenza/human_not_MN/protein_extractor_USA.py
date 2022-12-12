b8ffab362b8a8698cfd3b6bd2ae3b64ff6408197
belle172 <belle172@umn.edu>
2022-12-12 00:41:51 -0600

    create scripts

--- NCBI_genomes_influenza_genomeset_dat/genomeset_dat/human_influenza/human_not_MN/protein_extractor_USA.py
+++ NCBI_genomes_influenza_genomeset_dat/genomeset_dat/human_influenza/human_not_MN/protein_extractor_USA.py
@@ -1,12 +1,12 @@
 # this script takes an input file of sequences and a metadata criteria and creates 
 # a file of the sequences with the specified metadata, and a file of the sequences 
 # without the metadata 
-input_file = open('genomeset_dat.txt') 
-output = open('Human_influenza.txt', 'w') # file for genomes we are interested in 
-output_2 = open('non_human.txt', 'w') 
+input_file = open('human_not_MN.txt') 
+output = open('human_not_MN_USA.txt', 'w') # file for genomes we are interested in 
+output_2 = open('human_not_MN_not_USA.txt', 'w') 
 
 for line in input_file: # read in the data 
-    if 'Human' in line: 
+    if 'USA' in line: 
         output.write(line) 
 
     else: # sequence line 
--- NCBI_genomes_influenza_genomeset_dat/genomeset_dat/human_influenza/protein_extractor_minnesota.py
+++ NCBI_genomes_influenza_genomeset_dat/genomeset_dat/human_influenza/protein_extractor_minnesota.py
@@ -1,12 +1,12 @@
 # this script takes an input file of sequences and a metadata criteria and creates 
 # a file of the sequences with the specified metadata, and a file of the sequences 
 # without the metadata 
-input_file = open('genomeset_dat.txt') 
-output = open('Human_influenza.txt', 'w') # file for genomes we are interested in 
-output_2 = open('non_human.txt', 'w') 
+input_file = open('human_influenza.txt') 
+output = open('human_MN.txt', 'w') # file for genomes we are interested in 
+output_2 = open('human_not_MN.txt', 'w') 
 
 for line in input_file: # read in the data 
-    if 'Human' in line: 
+    if 'Minnesota' in line: 
         output.write(line) 
 
     else: # sequence line 
